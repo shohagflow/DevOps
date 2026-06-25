e79b8ca4baacb6842e7d4d519ba1ba97a0c4fc4a
Shohag <shohagflow@gmail.com>
2026-06-25 21:35:45 +0600

    Update component

---
+++ src/components/pages/Home.jsx
@@ -0,0 +1,19 @@
+import Btn from '../common/Btn'
+import './Home.css'
+
+function Home() {
+  return (
+    <div>
+      <h1>Home</h1>
+      <Btn/>  
+
+      <section className="home"> 
+        
+         </section>
+    </div>
+  )
+}
+
+export default Home
+
+
